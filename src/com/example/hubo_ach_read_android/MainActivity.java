2aebf6807f48cc4725cca2d239c7d3937d18b3d1
Daniel Lofaro <danlofaro.com>
2013-12-02 02:06:06 -0500

    playing with tables

--- src/com/example/hubo_ach_read_android/MainActivity.java
+++ src/com/example/hubo_ach_read_android/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.hubo_ach_read_android;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +13,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+     // reference the table layout
+        TableLayout tbl = (TableLayout)findViewById(R.id.RHE);
+        // delcare a new row
+        TableRow newRow = new TableRow(this);
+        // add views to the row
+        newRow.addView(new TextView(this)); // you would actually want to set properties on this before adding it
+        // add the row to the table layout
+        tbl.addView(newRow);
     }
 
 
